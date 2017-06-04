54c7556741ceb850e4217d097053762efccd5b59
Pawel Lampe <pawel.lampe@gmail.com>
2017-06-04 19:16:08 +0200

    TextBuffer thread-safety

--- README.md
+++ README.md
@@ -2,6 +2,5 @@
 
 ## TODO
  * proper CTRL+L/R handling
- * multi-threaded TextBuffer
  * source as static lib which is linked with main or with tests (CMakes refa)
  * (...)
--- source/TextBuffer.cpp
+++ source/TextBuffer.cpp
@@ -9,26 +9,31 @@ TextBuffer::TextBuffer() :
 
 unsigned TextBuffer::size()
 {
+  std::lock_guard<std::mutex> lock(mBufferLock);
   return mBuffer.size();
 }
 
 bool TextBuffer::isFull()
 {
+  std::lock_guard<std::mutex> lock(mBufferLock);
   return mFull;
 }
 
 void TextBuffer::applyFunctionToSlice(std::function<void(Iterator, Iterator)> function, unsigned pos, unsigned len)
 {
+  std::lock_guard<std::mutex> lock(mBufferLock);
   assert(pos <= mBuffer.size() && pos+len <= mBuffer.size());
   function(mBuffer.begin()+pos, mBuffer.begin()+pos+len);
 }
 
 void TextBuffer::appendData(const Buffer& data)
 {
+  std::lock_guard<std::mutex> lock(mBufferLock);
   mBuffer.insert(mBuffer.end(), data.begin(), data.end());
 }
 
 void TextBuffer::seal()
 {
+  std::lock_guard<std::mutex> lock(mBufferLock);
   mFull = true;
 }
