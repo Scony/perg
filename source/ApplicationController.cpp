d29d353b46fc5607cef14868d62c4dbaa7bd47a1
Pawel Lampe <pawel.lampe@gmail.com>
2017-06-16 21:46:33 +0200

    AppController refa

--- source/ApplicationController.cpp
+++ source/ApplicationController.cpp
@@ -1,9 +1,8 @@
 #include "ApplicationController.hpp"
-#include "File.hpp"
-#include "FileController.hpp"
 
 ApplicationController::ApplicationController(Region region) :
   mRegion(region),
+  mCurrentFile(-1),
   mStatusBar(new StatusBar(Region(0, region.rows-2, region.cols, 1))),
   mMinibuffer(new Minibuffer(Region(0, region.rows-1, region.cols, 1)))
 {
@@ -14,16 +13,25 @@ void ApplicationController::run()
   mStatusBar->render();
   mMinibuffer->render();
 
-  mMinibuffer->readStr();
+  Event event(-1);
+  while (event.key != 'q')
+    {
+      if (mCurrentFile >= 0)
+	event = mFileControllers[mCurrentFile]->proceed();
+      else
+	mMinibuffer->readStr();	// TODO: display init screen
+    }
 }
 
 void ApplicationController::openFile(const std::string& filePath)
 {
   // TODO: check if file exists ?
-  auto file = std::shared_ptr<File>(new File(filePath));
+  std::shared_ptr<File> file(new File(filePath));
   std::shared_ptr<FileController> fileController(new FileController(Region(0, 0, mRegion.cols, mRegion.rows-2),
 								    file,
 								    mStatusBar,
 								    mMinibuffer));
-  fileController->proceed();
+  mFiles.insert({file->getGid(), file});
+  mFileControllers.insert({file->getGid(), fileController});
+  mCurrentFile = file->getGid();
 }
--- source/ApplicationController.hpp
+++ source/ApplicationController.hpp
@@ -2,10 +2,13 @@
 
 #include <string>
 #include <memory>
+#include <map>
 
 #include "StatusBar.hpp"
 #include "Minibuffer.hpp"
 #include "Region.hpp"
+#include "File.hpp"
+#include "FileController.hpp"
 
 class ApplicationController
 {
@@ -18,6 +21,10 @@ public:
 
 private:
   Region mRegion;
+
+  std::map<unsigned, std::shared_ptr<File> > mFiles;
+  std::map<unsigned, std::shared_ptr<FileController> > mFileControllers;
+  int mCurrentFile;
 
   std::shared_ptr<StatusBar> mStatusBar;
   std::shared_ptr<Minibuffer> mMinibuffer;
